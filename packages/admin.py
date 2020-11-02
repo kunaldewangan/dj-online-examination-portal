7c4e0310f123641157b970351880f6fef136d1d2
KetanZode <ketanzode2104@gmail.com>
2020-11-02 16:16:45 +0530

    admin site(packages) updated

--- packages/admin.py
+++ packages/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from . import models
+from django.contrib.admin.options import ModelAdmin
+
 # Register your models here.
 
-admin.site.register(models.Package)
-admin.site.register(models.PackageStudent)
+
+class PackageAdmin(ModelAdmin):
+    list_display = ["name","no_of_Q","total_marks","duration"]
+    list_filter = ["name","no_of_Q","total_marks"]
+    search_fields = ["name","description","date"]
+
+class PackageStudentAdmin(ModelAdmin):
+    list_display = ["student","package","marks"]
+    list_filter =  ["package","student__username"]
+    search_fields = ["package__name","marks","student__username"]
+
+
+
+
+
+admin.site.register(models.Package,PackageAdmin)
+admin.site.register(models.PackageStudent,PackageStudentAdmin)
