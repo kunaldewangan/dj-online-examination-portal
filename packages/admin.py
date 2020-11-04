fc0495f12c3c7202c23a6c51453218d2e5d5a4bb
kunaldewangan <dkunal3333@gmail.com>
2020-11-04 23:03:05 +0530

    packages admin update

--- packages/admin.py
+++ packages/admin.py
@@ -6,13 +6,13 @@ from django.contrib.admin.options import ModelAdmin
 
 
 class PackageAdmin(ModelAdmin):
-    list_display = ["name","no_of_Q","total_marks","duration"]
-    list_filter = ["name","no_of_Q","total_marks"]
-    search_fields = ["name","description","date"]
+    list_display = ["name","no_of_Q","start_time","end_time","visibility","q_visibility","total_marks","duration"]
+    list_filter = ["name","visibility","q_visibility","no_of_Q","total_marks"]
+    search_fields = ["name","description","start_time","end_time"]
 
 class PackageStudentAdmin(ModelAdmin):
-    list_display = ["student","package","marks"]
-    list_filter =  ["package","student__username"]
+    list_display = ["student","package","marks","submission"]
+    list_filter =  ["package","student__username","submission"]
     search_fields = ["package__name","marks","student__username"]
 
 
