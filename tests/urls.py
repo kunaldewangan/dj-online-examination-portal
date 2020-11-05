c51d1ce862b264dd847d09cfa9beb156cb4ee006
kunaldewangan <dkunal3333@gmail.com>
2020-11-05 12:47:34 +0530

    test url update

--- tests/urls.py
+++ tests/urls.py
@@ -5,5 +5,6 @@ app_name = 'tests'
 
 urlpatterns = [
     path('<int:package_id>',views.TestView,name='test'),
-    path('successful/',generic.TemplateView.as_view(template_name='tests/test_successful.html'),name='test_successful')
+    path('successful/',generic.TemplateView.as_view(template_name='tests/test_successful.html'),name='test_successful'),
+    path('denied/',generic.TemplateView.as_view(template_name='tests/not_permitted.html'),name='test_denied'),
 ]
